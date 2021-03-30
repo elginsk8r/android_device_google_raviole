a367344450f5cbfe206af1305f7e677563e082fb
Alessandro Astone <ales.astone@gmail.com>
2021-03-30 11:34:38 +0200

    Build PowerShare HAL
    Change-Id: I3c0321b844acb5f97ac398acc842501410a44b6c

--- device-evervolv.mk
+++ device-evervolv.mk
@@ -8,6 +8,9 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# PowerShare
+include hardware/google/pixel/powershare/device.mk
+
 # Build necessary packages for vendor
 
 # Audio
