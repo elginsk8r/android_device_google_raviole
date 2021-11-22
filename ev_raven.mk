7a2c609b60d6af7e3a10a6e561e641f8e6f2950d
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-22 21:22:09 +0530

    Add initial Lineage product makefiles
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6
    
    Update fingerprint/build description from SQ3A.220605.009.B1
    
    Change-Id: Iaf930a8f9c64a7a5a449fa6f4fc0a7aa9c6a1c5b
    
    Update fingerprint/build description from SQ3A.220705.003.A1
    
    Change-Id: I4aed2529774a38edb184f06e7bfe3388edc92f0e

---
+++ ev_raven.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Evervolv stuff.
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/raviole/aosp_raven.mk)
+
+$(call inherit-product, device/google/gs101/evervolv_common.mk)
+$(call inherit-product, device/google/raviole/device-evervolv.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 6 Pro
+PRODUCT_NAME := ev_raven
+
+# Boot animation
+BOOT_ANIMATION_SIZE := 1440p
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=raven \
+    PRIVATE_BUILD_DESC="raven-user 13 TP1A.220905.004 8927612 release-keys" \
+    BUILD_THUMBPRINT="13/TP1A.220905.004/8927612:user/release-keys"
+
+BUILD_FINGERPRINT := google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys
